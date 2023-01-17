3bde736278c270b1113e00fc62ae4b9fa391f37f
Nikolaj Hoggins <mollevip@gmail.com>
2023-01-17 14:38:29 +0100

    Cleanup

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,19 +1,9 @@
 import { Command } from './Command';
 import { Based } from './commands/Based';
 import { EndGame } from './commands/EndGame';
-import { Hello } from './commands/Hello';
 import { QueueCommand } from './commands/Queue';
 import { Ready } from './commands/Ready';
 import { Stats } from './commands/Stats';
 import { Top } from './commands/Top';
 import { Unready } from './commands/Unready';
-export const Commands: Command[] = [
-    Hello,
-    Based,
-    Stats,
-    Ready,
-    Top,
-    QueueCommand,
-    Unready,
-    EndGame,
-];
+export const Commands: Command[] = [Based, Stats, Ready, Top, QueueCommand, Unready, EndGame];
