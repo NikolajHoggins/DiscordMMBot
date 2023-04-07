17b23f45af6c2b8db063d1b712935d02272ddcd6
Nikolaj Hoggins <mollevip@gmail.com>
2023-04-07 21:23:18 +0200

    Helper for creating and deleting channels

---
+++ src/helpers/channel.ts
@@ -0,0 +1,60 @@
+import { Channel, Client, PermissionsBitField } from 'discord.js';
+import { getEveryoneRole, getGuild } from './guild.js';
+
+export const createChannel = ({
+    client,
+    name,
+    parentId,
+    allowedIds,
+}: {
+    client: Client;
+    name: string;
+    allowedIds: string[];
+    parentId?: string;
+}): Promise<Channel> => {
+    return new Promise(async resolve => {
+        const guild = await getGuild(client);
+
+        const everyoneRole = await getEveryoneRole(client);
+
+        const channel = await guild.channels.create({
+            name: name,
+            permissionOverwrites: [
+                {
+                    id: everyoneRole.id,
+                    deny: [PermissionsBitField.Flags.ViewChannel],
+                },
+                ...allowedIds.map(id => ({
+                    id: id,
+                    allow: [PermissionsBitField.Flags.ViewChannel],
+                })),
+                ...(process.env.MOD_ROLE_ID
+                    ? [
+                          {
+                              id: process.env.MOD_ROLE_ID,
+                              allow: [PermissionsBitField.Flags.ViewChannel],
+                          },
+                      ]
+                    : []),
+            ],
+            ...(parentId ? { parent: parentId } : {}),
+        });
+        if (!channel) throw new Error("Couldn't create channel");
+
+        resolve(channel);
+    });
+};
+
+export const deleteChannel = async ({
+    client,
+    channelId,
+}: {
+    client: Client;
+    channelId: string;
+}) => {
+    const guild = await getGuild(client);
+
+    const channel = await guild.channels.fetch(channelId);
+    if (!channel) throw new Error("Couldn't find channel");
+    await channel.delete();
+};
