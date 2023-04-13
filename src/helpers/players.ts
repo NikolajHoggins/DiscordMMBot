25c136d7a385d0cd98706a1cd68c2ce629318b3d
Nikolaj Hoggins <mollevip@gmail.com>
2023-04-13 18:01:26 +0200

    Create balanced teams :pog:

--- src/helpers/players.ts
+++ src/helpers/players.ts
@@ -1,19 +1,47 @@
 import { shuffle } from 'lodash';
 import { IQueue } from '../models/queue.schema';
 import { IMatchPlayer } from '../models/match.schema.js';
+import { IPlayer } from '../models/player.schema.js';
 
 export interface ICreateTeamsResponse {
     teamA: string[];
     teamB: string[];
 }
+
+export const splitObjects = (players: IQueue[]) => {
+    const sortedObjects = players.slice().sort((a, b) => b.rating - a.rating);
+    const group1 = [];
+    const group2 = [];
+
+    for (const obj of sortedObjects) {
+        if (
+            group1.reduce((sum, o) => sum + o.rating, 0) <=
+            group2.reduce((sum, o) => sum + o.rating, 0)
+        ) {
+            group1.push(obj);
+        } else {
+            group2.push(obj);
+        }
+    }
+
+    return [group1, group2];
+};
+
 export const createTeams = (queuePlayers: IQueue[]): IMatchPlayer[] => {
     const players = shuffle(queuePlayers);
-    const teamA: IMatchPlayer[] = players
-        .slice(0, players.length / 2)
-        .map(q => ({ id: q.discordId, team: 'a', name: q.name, rating: q.rating }));
-    const teamB: IMatchPlayer[] = players
-        .slice(players.length / 2, players.length)
-        .map(q => ({ id: q.discordId, team: 'b', name: q.name, rating: q.rating }));
+    const fairTeams = splitObjects(players);
+    const teamA: IMatchPlayer[] = fairTeams[0].map(q => ({
+        id: q.discordId,
+        team: 'a',
+        name: q.name,
+        rating: q.rating,
+    }));
+    const teamB: IMatchPlayer[] = fairTeams[1].map(q => ({
+        id: q.discordId,
+        team: 'b',
+        name: q.name,
+        rating: q.rating,
+    }));
 
     return [...teamA, ...teamB];
 };
