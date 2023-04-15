4783f14e859ecc16f87e68ed459a4d0fd18c1d0b
Nikolaj Hoggins <mollevip@gmail.com>
2023-04-15 03:04:23 +0200

    Show regions in queue

--- src/commands/Queue.ts
+++ src/commands/Queue.ts
@@ -1,6 +1,7 @@
 import { CommandInteraction, Client, ApplicationCommandType } from 'discord.js';
 import { Command } from '../Command';
 import Queue from '../models/queue.schema';
+import { groupBy, map, upperCase } from 'lodash';
 
 export const QueueCommand: Command = {
     name: 'queue',
@@ -10,11 +11,17 @@ export const QueueCommand: Command = {
         //Fetch user from database
         const queuePlayers = await Queue.find();
 
+        const regions = groupBy(queuePlayers.map(p => p.region));
+
+        const regionString = map(regions, (value, key) => {
+            return `${value.length} - ${upperCase(key)}`;
+        }).join(', ');
+
         let content = 'Currently looking for a game: ';
         queuePlayers.forEach(queue => {
             content = `${content} ${queue.name},`;
         });
-        content = `[${queuePlayers.length}] - ${content}`;
+        content = `[${queuePlayers.length}] - ${content}  ${regionString}`;
 
         await interaction.reply({
             content,
