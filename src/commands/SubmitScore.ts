c4341409f0d0762c3a95cd9f1025c661b8a3fd56
Nikolaj Hoggins <mollevip@gmail.com>
2023-09-11 21:50:53 +0200

    Fixed score submit

--- src/commands/SubmitScore.ts
+++ src/commands/SubmitScore.ts
@@ -19,7 +19,7 @@ export const SubmitScore: Command = {
             name: 'score',
             description: 'score of your own team',
             type: ApplicationCommandOptionType.Number,
-            max_value: 9,
+            max_value: 11,
             min_value: 0,
             required: true,
         },
--- src/commands/mod/ForceSubmit.ts
+++ src/commands/mod/ForceSubmit.ts
@@ -30,7 +30,7 @@ export const ForceSubmit: Command = {
             name: 'score',
             description: 'rounds won by the players team',
             min_value: 0,
-            max_value: 9,
+            max_value: 11,
             required: true,
         },
     ],
