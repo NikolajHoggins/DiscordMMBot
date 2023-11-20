b2d6b461ed80fe7426569de660aeda730857021f
Hoggins <nikolajhjacobsen@gmail.com>
2023-11-20 23:16:41 +0100

    Cowboy fix for keeping first ban tickdown 3 days and subsequent 24 hours

--- src/crons/banTickDown.ts
+++ src/crons/banTickDown.ts
@@ -8,6 +8,7 @@ export const runBanTickDown = async (client: Client) => {
     const now = Date.now();
 
     const HOURS_24 = 24 * 60 * 60 * 1000;
+    const DAYS_2 = HOURS_24 * 2;
     const DAYS_3 = HOURS_24 * 3;
 
     //Get all users with a ban multiplier
@@ -19,11 +20,13 @@ export const runBanTickDown = async (client: Client) => {
     for (const i in players) {
         const player = players[i];
 
+        //Cowboy fix for having first ban tickdown happen after 3 days and then all following be after 24 hours
+        const NEXT_BAN_TICK_DOWN = now - DAYS_2;
         //Tick down ban multiplier
         const newBanMultiplier = player.banMultiplier - 1;
         await Player.updateOne(
             { discordId: player.discordId },
-            { banMultiplier: newBanMultiplier, banTickDown: now }
+            { banMultiplier: newBanMultiplier, banTickDown: NEXT_BAN_TICK_DOWN }
         );
     }
 };
