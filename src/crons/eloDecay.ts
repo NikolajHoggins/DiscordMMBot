184be0abff0d03831b724d9a9084d5b05f93426c
Hoggins <nikolajhjacobsen@gmail.com>
2023-12-10 01:05:03 +0100

    Delete elo decay

--- src/crons/eloDecay.ts
+++ src/crons/eloDecay.ts
@@ -7,56 +7,56 @@ import { ChannelsType } from '../types/channel';
 
 export const runEloDecay = async (client: Client) => {
     return;
-    console.log('Running elo decay cron');
-    if (!client.user) return;
+    // console.log('Running elo decay cron');
+    // if (!client.user) return;
 
-    const now = Date.now();
+    // const now = Date.now();
 
-    const HOURS_24 = 24 * 60 * 60 * 1000;
-    const DAYS_10 = HOURS_24 * 10;
+    // const HOURS_24 = 24 * 60 * 60 * 1000;
+    // const DAYS_10 = HOURS_24 * 10;
 
-    //Get all users afk for more than 10 days
-    const players = await Player.find({
-        lastMatch: { $lt: now - DAYS_10 },
-    });
+    // //Get all users afk for more than 10 days
+    // const players = await Player.find({
+    //     lastMatch: { $lt: now - DAYS_10 },
+    // });
 
-    console.log(`removing elo from ${players.length} players`);
-    for (const i in players) {
-        const player: IPlayer = players[i];
+    // console.log(`removing elo from ${players.length} players`);
+    // for (const i in players) {
+    //     const player: IPlayer = players[i];
 
-        if (!player.lastMatch) continue;
+    //     if (!player.lastMatch) continue;
 
-        const daysSinceLastMatch = Math.round((now - player.lastMatch) / HOURS_24);
+    //     const daysSinceLastMatch = Math.round((now - player.lastMatch) / HOURS_24);
 
-        // Calculate elo loss as 5 + 0.1 for every day over 10
-        const daysOverTen = Math.max(0, daysSinceLastMatch - 10);
-        const eloChange = -1 * (5 + 0.1 * daysOverTen);
+    //     // Calculate elo loss as 5 + 0.1 for every day over 10
+    //     const daysOverTen = Math.max(0, daysSinceLastMatch - 10);
+    //     const eloChange = -1 * (5 + 0.1 * daysOverTen);
 
-        // Tick down elo
-        await Player.updateOne(
-            { discordId: player.discordId },
-            {
-                $inc: { rating: eloChange },
-                $push: {
-                    ratingHistory: {
-                        rating: player.rating + eloChange,
-                        date: Date.now(),
-                        reason: `${daysSinceLastMatch} days since last match`,
-                    },
-                },
-            }
-        );
+    //     // Tick down elo
+    //     await Player.updateOne(
+    //         { discordId: player.discordId },
+    //         {
+    //             $inc: { rating: eloChange },
+    //             $push: {
+    //                 ratingHistory: {
+    //                     rating: player.rating + eloChange,
+    //                     date: Date.now(),
+    //                     reason: `${daysSinceLastMatch} days since last match`,
+    //                 },
+    //             },
+    //         }
+    //     );
 
-        //get queue channel id
-        const queueChannelId = await getChannelId(ChannelsType['ranked-queue']);
-        sendMessage({
-            channelId: queueChannelId,
-            messageContent: `<@${player.discordId}> lost ${
-                eloChange * -1
-            } elo for being inactive for ${daysSinceLastMatch} days`,
-            client,
-        });
-    }
+    //     //get queue channel id
+    //     const queueChannelId = await getChannelId(ChannelsType['ranked-queue']);
+    //     sendMessage({
+    //         channelId: queueChannelId,
+    //         messageContent: `<@${player.discordId}> lost ${
+    //             eloChange * -1
+    //         } elo for being inactive for ${daysSinceLastMatch} days`,
+    //         client,
+    //     });
+    // }
 };
 
 const initEloDecayCron = async (client: Client) => {
