53f9a8997630cbc7ad9153a0a2207fe48c00b4cd
Hoggins <nikolajhjacobsen@gmail.com>
2024-02-13 14:18:38 +0100

    2 day till tickdown

--- src/crons/banTickDown.ts
+++ src/crons/banTickDown.ts
@@ -9,13 +9,13 @@ export const runBanTickDown = async (client: Client) => {
     const now = Date.now();
 
     const HOURS_24 = 24 * 60 * 60 * 1000;
+    const DAYS_1 = HOURS_24 * 1;
     const DAYS_2 = HOURS_24 * 2;
-    const DAYS_3 = HOURS_24 * 3;
 
     //Get all users with a ban multiplier
     const players = await Player.find({
         banMultiplier: { $gt: 0 },
-        banTickDown: { $lt: now - DAYS_3 },
+        banTickDown: { $lt: now - DAYS_2 },
     });
 
     for (const i in players) {
@@ -28,7 +28,7 @@ export const runBanTickDown = async (client: Client) => {
         });
 
         //Cowboy fix for having first ban tickdown happen after 3 days and then all following be after 24 hours
-        const NEXT_BAN_TICK_DOWN = now - DAYS_2;
+        const NEXT_BAN_TICK_DOWN = now - DAYS_1;
         //Tick down ban multiplier
         const newBanMultiplier = player.banMultiplier - 1;
         await Player.updateOne(
