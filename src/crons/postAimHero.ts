cdf8e7e0b4feabf5e2a370b62386b1bd9fb8bc31
Hoggins <nikolajhjacobsen@gmail.com>
2025-09-07 13:39:28 +0200

    Less often ads

--- src/crons/postAimHero.ts
+++ src/crons/postAimHero.ts
@@ -27,7 +27,7 @@ export const postAimHero = async (client: Client) => {
 
 const initPostAimHeroCron = async (client: Client) => {
     // Every hour at minute 0
-    cron.schedule('0 * * * *', async () => {
+    cron.schedule('0 */2 * * *', async () => {
         postAimHero(client);
     });
 };
